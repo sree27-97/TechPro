1444d8898d1a94c93a4d96da6c45eff86fe8182e
Sreelakshmi <slakshmi1997@gmail.com>
2020-03-26 14:49:57 +0530

    first asigment

---
+++ GitAssigment/src/com/demo/Demo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.Scanner;
+
+import javax.jws.soap.SOAPBinding;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the name:");
+		String str=sc.next();
+		
+	}
+
+}
