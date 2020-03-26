4e4fd2bf797e71be5272105b0394e3b536d4bec1
sree27-97 <62684015+sree27-97@users.noreply.github.com>
2020-03-26 14:55:02 +0530

    Update Demo.java

--- GitAssigment/src/com/demo/Demo.java
+++ GitAssigment/src/com/demo/Demo.java
@@ -9,8 +9,10 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the name:");
+		System.out.println("enter the first name:");
 		String str=sc.next();
+		System.out.println("enter gender");
+		int age=sc.next();
 		
 	}
 
